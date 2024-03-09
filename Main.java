2353c69005b18ad8f3f47ea866a5495a8c317fe6
KingOfNooks <seandthaxton1@gmail.com>
2024-03-08 23:35:35 -0500

    Half way done with the initial setup

--- Main.java
+++ Main.java
@@ -1,5 +1,25 @@
+// Imports
+import java.util.Scanner;
+
+//Main Program
 class Main{
     public static void main(String[] args) {
-        
+
+        //The inital setup for the program
+        boolean start = true;
+        Scanner theScan = new Scanner(System.in);
+
+        //continuously asking user for a correct date
+        while (start) {
+            System.out.println("Give me a date in the format of MM/DD/YYYY");
+            String user = theScan.nextLine();
+
+            try {
+                
+            } catch (Exception e) {
+                // TODO: handle exception
+            }
+        }
+
     }
 }
